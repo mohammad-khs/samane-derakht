003cfeaf14e8561df6ec2367d99a351a35897e58
mohammad-khs <101863197+mohammad-khs@users.noreply.github.com>
2024-10-19 23:35:59 +0330

    made small changes on storyModal

--- src/components/ui/main/storyModal.tsx
+++ src/components/ui/main/storyModal.tsx
@@ -99,7 +99,7 @@ const StoryModal: React.FC<StoryModalProps> = ({
           </div>
           <div className="relative carousel-item text-center py-2 h-[650px] w-[360px]">
             <button
-              className="absolute my-2 z-50 right-2 hover:text-black transition-colors text-gray-800"
+              className="absolute my-2 z-50 right-2 text-white"
               onClick={onClose}
             >
               <ArrowRight className="h-8 w-8" />
@@ -120,20 +120,19 @@ const StoryModal: React.FC<StoryModalProps> = ({
                 <div className="absolute my-2 right-12 bg-white w-8 h-8 rounded-full">
                   <TreeIcon imageName={imageData.story_name} />
                 </div>
-                <div className="absolute mx-2 z-50 h-full flex justify-center items-center flex-col">
-                  <EyeIcon />
-                  <p className="">{imageData?.seen_numbers}</p>
+                <div className="absolute mx-2 z-50 h-full flex justify-end items-center flex-col">
+                  <div className="mb-32 flex items-center flex-col">
+                    <EyeIcon className="text-white" />
+                    <p className="text-white">{imageData?.seen_numbers}</p>
+                  </div>
                 </div>
               </>
             ) : (
               <>
                 <div className="w-full h-full z-40 bg-slate-700 flex justify-center items-center">
                   <FrownIcon className="h-20 w-20" />
-                  <div className="absolute top-0 left-0 mx-2 z-50 h-full flex items-center">
-                    <div className="flex items-center flex-col">
-                      <EyeIcon />
-                      <p className="">{imageData?.seen_numbers}</p>
-                    </div>
+                  <div className="absolute top-0 left-0 mx-2 z-50 h-full flex items-end">
+                    <EyeIcon className="text-white mb-36" />
                   </div>
                 </div>
               </>
