ae4d1580ada4195fa90d407a6f42966f9b8ecb33
mohammad-khs <101863197+mohammad-khs@users.noreply.github.com>
2025-04-07 22:35:38 +0330

    made mobile-nav change color on active

--- src/components/mobile-nav.tsx
+++ src/components/mobile-nav.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from "react";
 import {
   Sheet,
@@ -19,12 +21,15 @@ import Link from "next/link";
 
 import { FaQuestion } from "react-icons/fa";
 import SignInModalParent from "./authentication/signInModalParent";
+import { usePathname } from "next/navigation";
 
 interface MobileNavProps {
   disappearLg?: boolean;
 }
 
 export default function MobileNav({ disappearLg = false }: MobileNavProps) {
+  const pathname = usePathname();
+
   return (
     <Sheet>
       <SheetDescription>
@@ -56,54 +61,73 @@ export default function MobileNav({ disappearLg = false }: MobileNavProps) {
           <VisuallyHidden.Root>menu</VisuallyHidden.Root>
         </SheetTitle>
         <nav className="grid gap-2 text-lg font-medium" dir="rtl">
-          <Link
-            href="#"
-            className="flex items-center gap-2 text-lg font-semibold"
-          >
-            <Button
-              className="bg-[#28D16C] border-none"
-              variant={"icon"}
-              size={"resizbleIcon"}
-            ></Button>
-            <span className="sr-only">سامانه درخت</span>
-          </Link>
-          <Link
-            href="/products"
-            className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-          >
-            <Package2 className="h-5 w-5" />
-            محصولات
-          </Link>
-          <Link
-            href="/shopping-cart"
-            className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-          >
-            <ShoppingCart className="h-5 w-5" />
-            سبد خرید
-          </Link>
-
-          <Link
-            href="/watering-trees"
-            className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-          >
-            <Droplets className="h-5 w-5" />
-            آبیاری درختان
-          </Link>
-          <Link
-            href="/company/faq"
-            className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-          >
-            <FaQuestion className="h-5 w-5" />
-            سوالات متداول
-          </Link>
-          <Link
-            href="/company/about-us"
-            className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-          >
-            <InfoIcon className="h-5 w-5" />
-            درباره ما
-          </Link>
-        </nav>
+        <Link
+          href="#"
+          className="flex items-center gap-2 text-lg font-semibold"
+        >
+          <Button
+            className="bg-[#28D16C] border-none"
+            variant={"icon"}
+            size={"resizbleIcon"}
+          ></Button>
+          <span className="sr-only">سامانه درخت</span>
+        </Link>
+        <Link
+          href="/products"
+          className={`mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 ${
+            pathname === "/products"
+              ? "text-[#28D16C] hover:text-[#28D16C]"
+              : "text-muted-foreground hover:text-foreground"
+          }`}
+        >
+          <Package2 className="h-5 w-5" />
+          محصولات
+        </Link>
+        <Link
+          href="/shopping-cart"
+          className={`mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 ${
+            pathname === "/shopping-cart"
+              ? "text-[#28D16C] hover:text-[#28D16C]"
+              : "text-muted-foreground hover:text-foreground"
+          }`}
+        >
+          <ShoppingCart className="h-5 w-5" />
+          سبد خرید
+        </Link>
+        <Link
+          href="/watering-trees"
+          className={`mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 ${
+            pathname === "/watering-trees"
+              ? "text-[#28D16C] hover:text-[#28D16C]"
+              : "text-muted-foreground hover:text-foreground"
+          }`}
+        >
+          <Droplets className="h-5 w-5" />
+          آبیاری درختان
+        </Link>
+        <Link
+          href="/company/faq"
+          className={`mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 ${
+            pathname === "/company/faq"
+              ? "text-[#28D16C] hover:text-[#28D16C]"
+              : "text-muted-foreground hover:text-foreground"
+          }`}
+        >
+          <FaQuestion className="h-5 w-5" />
+          سوالات متداول
+        </Link>
+        <Link
+          href="/company/about-us"
+          className={`mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 ${
+            pathname === "/company/about-us"
+              ? "text-[#28D16C] hover:text-[#28D16C]"
+              : "text-muted-foreground hover:text-foreground"
+          }`}
+        >
+          <InfoIcon className="h-5 w-5" />
+          درباره ما
+        </Link>
+      </nav>
       </SheetContent>
     </Sheet>
   );
